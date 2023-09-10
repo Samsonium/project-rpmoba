4f23d3551a8c29fad2a23a729d0f8f0b7fc51ee8
Samsonium <tdesu@vk.com>
2023-09-10 20:49:36 +0300

    Implement Construct type

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+import {Asset} from './engine/asset-manager';
+
+/**
+ * Constructor type
+ */
+type Construct<T> = new (...args: any[]) => T extends Asset ? T : never;
